983c7049bdb3c7198e1ee631cad3c3f5f7d8acef
Miroslav Lucic <you@example.com>
2019-09-27 17:27:18 +0200

    petak

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,30 +5,46 @@ import './Header.css';
 class Header extends Component {
     render() {
         return(
-            <div className="container">
-                
-                    <nav className="navbar navbar-expand-lg row justify-content-md-center">
-                        <div className="navbar-brand col-3"><img className="logo" src="./img/logo.png" alt='Logo'>
-                            </img></div>
+            <div className="background1">
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light h-100 px-0 pb-lg-0">
 
-                        <div className="collapse navbar-collapse col-9" id="navbarSupportedContent">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">HOME</li>
-                                <li className="nav-item">ABOUT US</li>
-                                <li className="nav-item">SERVICES</li>
-                                <li className="nav-item">ACHIEVEMENTS</li>
-                                <li className="nav-item">PRODUCTS</li>
-                                <li className="nav-item">CONTACT</li>
-                            </ul>
-                        </div>
+                        <a className="navbar-brand" href="#about"><img src="./img/logo.png" alt="Logo"/></a>
 
-                    </nav>
-         
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-button" aria-controls="menu-button" aria-expanded="false" aria-label="Toggle navigation border-radius-1">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="menu-button">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link  active" href="home">HOME
+                                        <span className="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#about">ABOUT US</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#services">SERVICES</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#achievement">ACHIEVEMENT</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#products">PRODUCTS</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#contact">CONTACT</a>
+                                </li>
+                           </ul>
+                                
+                  </div>
+                </nav>
+                </div>
             </div>
-
-            
         )
     }
 }
 
-export {Header};
+export {Header}
---
+++ src/components/Home/Home.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+
+class Home extends Component {
+	render() {
+		return(
+
+			<div>
+				<section id="home-section" className="lead-section">
+					<div className="carousel slide" id="lead-slider">
+						<div className="carousel-inner">
+
+							<div className="carousel-item active">
+								<div className="container">
+									<div className="caption p-4 bg-dark text-white">
+									<h2>Choose your sunglasses</h2>
+									</div>
+								</div>
+							</div>
+
+							<div className="carousel-item">
+								<div className="container">
+									<div className="caption p-4 bg-dark text-white">
+									<h2>Choose your sunglasses</h2>
+									</div>
+								</div>
+							</div>
+
+							<div className="carousel-item">
+								<div className="container">
+									<div className="caption p-4 bg-dark text-white">
+									<h2>Choose your sunglasses</h2>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+						<div className="container position-relative">
+							<ol className="carousel-indicators">
+							    <li data-target="#lead-slider" data-slide-to="0" className="active"></li>
+							    <li data-target="#lead-slider" data-slide-to="1"></li>
+							    <li data-target="#lead-slider" data-slide-to="2"></li>
+							</ol>
+						</div>
+					</div>
+				</section>
+			</div>
+		)
+	}
+}
+
+export {Home};
